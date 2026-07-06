9a574647a3b01ab2bbc86f1e3da86908f8412d46
Tejas Dhawale <tejasdhawale145@gmail.com>
2026-07-06 11:45:34 +0530

    feat: integrated ml recommendation engine and configure docker volumes for backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,40 @@
 import os
 import psycopg
+import json
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from app.db import Base, engine
 from app import models
 from app.api.v1.auths import router as auth_router
 from app.api.v1.apps import router as app_router
+from app.recommender import Recommender
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan context manager for FastAPI application."""
+    # Perform any startup tasks here
+    print("loading the dataset ")
+    
+    # loading the dataset and initializing the recommendation model
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    dataset_path = os.path.join(base_dir, "../seed/archive/full_format_recipes.json")
+    
+    with open(dataset_path, "r") as f:
+        records = json.load(f)
+    
+    app.state.recommender = Recommender.from_records(records)
+    
+    print("recommendation model ready")
+    
+    yield  # This allows the application to run
+    
+    #shutdown logic
+    print("shutting down the application")
+    app.state.recommendation_model = None  
+    
+# pass the lifespan context manager to the FastAPI application
+app = FastAPI(lifespan=lifespan)
 
 Base.metadata.create_all(bind=engine)
 
